63d6b5915e20e2ade6bfb98efde95cec966383f0
Arc <arclight04@gmail.com>
2024-06-13 11:09:11 +0800

    cleaned migration files

--- backend/database/migrations/2024_04_10_031055_create_logs_table.php
+++ backend/database/migrations/2024_04_10_031055_create_logs_table.php
@@ -12,8 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('logs', function (Blueprint $table) {
-            $table->id('logs_id'); //Modifying the name of the primary key
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); //Pwedeng maglagay ng onUpdate('cascade') pero para sa unique ID lng yon na maupdate
+            $table->id('logs_id'); // Modifying the name of the primary key
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade'); // user_id can be nullable
+            $table->unsignedBigInteger('admin_id')->nullable();
+            $table->foreign('admin_id')->references('admin_id')->on('admin')->onDelete('cascade'); // Assuming the table name is 'admins'
             $table->string('action');
             $table->timestamps();
         });
