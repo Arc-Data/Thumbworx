0c59968ec41f5505e7a7aa4118c7ac7327b050c1
Arc <arclight04@gmail.com>
2024-06-27 16:01:41 +0800

    remodel step 2

--- frontend/src/views/main/Registration.vue
+++ frontend/src/views/main/Registration.vue
@@ -3,13 +3,6 @@
     <div class="max-w-3xl py-8 mx-auto">
       <div class="p-6 rounded-lg shadow-md bg-background-100">
         
-        <step2 v-else-if="step === 2"
-          :permanent_address="form.permanent_address"
-          :current_address="form.current_address"
-          @prevStep="prevStep"
-          @submit-step2="handleStep2">
-         
-        </step2>
         <div v-else-if="step === 3">
           <form @submit.prevent="submitForm">
 
@@ -73,41 +66,50 @@
     </div> 
   </div>-->
   <div class="flex min-h-screen bg-background-default">
-    <div class="hidden md:block w-[600px] bg-primary-200"></div>
+    <div class="hidden md:block w-[600px] bg-primary-800"></div>
     <div class="flex-1 px-4 py-10 bg-primary-1 00 ">
       <h1 class="text-2xl font-bold text-center">Sign up to Thumbworx</h1>
       <div class="container max-w-2xl mx-auto mt-4">
-        <ol class="flex items-center justify-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
-            <li class="flex md:w-full items-center text-primary-default dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-                <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-accent-default ">
-                  <svg v-if="step > 1" class="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                  </svg>
-                  <span v-else class="me-2">1</span>
-                  Personal <span class="hidden sm:inline-flex sm:ms-2">Info</span>
-                </span>
-            </li>
-            <li class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-                <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-                    <svg v-if="step > 2" class="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <ol class="flex items-center justify-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
+              <li 
+                :class="step > 1 ? 'text-primary-default' : 'text-gray-500' "
+                class="flex md:w-full items-center sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-primary-default after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 ">
+                  <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-primary-default ">
+                    <svg v-if="step > 1" class="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                     </svg>
-                    <span class="me-2">2</span>
-                    Account <span class="hidden sm:inline-flex sm:ms-2">Info</span>
-                </span>
-            </li>
-            <li class="flex items-center">
-                <span class="me-2">3</span>
-                Confirmation
-            </li>
-        </ol>
+                    <span v-else class="me-2">1</span>
+                    Personal <span class="hidden sm:inline-flex sm:ms-2">Info</span>
+                  </span>
+              </li>
+              <li 
+                :class="step > 2 ? 'text-primary-default' : 'text-gray-500'"
+                class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-primary-default after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10">
+                  <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-primary-default-200 ">
+                      <svg v-if="step > 2" class="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                      </svg>
+                      <span class="me-2">2</span>
+                      Account <span class="hidden sm:inline-flex sm:ms-2">Info</span>
+                  </span>
+              </li>
+              <li class="flex items-center">
+                  <span class="me-2">3</span>
+                  Confirmation
+              </li>
+          </ol>
       </div>
       <div class="px-8 md:px-20">
         <step1 v-if="step === 1"
-        :personal_info="form.personal_info"
-        :errors="errors.value"
-        :disabled="disabled"
-        @submit-step1="handleStep1"></step1>
+          :personal_info="form.personal_info"
+          :errors="errors.value"
+          :disabled="disabled"
+          @next-step="handleStep1"></step1>
+        <step2 v-else-if="step === 2"
+          :permanent_address="form.permanent_address"
+          :current_address="form.current_address"
+          @prev-step="prevStep"
+          @next-step="handleStep2"></step2>
       </div>
     </div>
   </div>
--- frontend/src/views/main/registration/Step2.vue
+++ frontend/src/views/main/registration/Step2.vue
@@ -1,95 +1,127 @@
 <template>
-    <div>
-        <h2 class="mb-4 text-xl font-bold">Step 2: Additional Information </h2>
-        <form @submit.prevent="handleNextStep">
-            <div class="mb-4">
-                <p class="block text-gray-700">Permanent Address</p>
-                <div class="mb-4">
-                    <label class="block text-gray-700">House Number</label>
-                    <input type="text" v-model="permanent_address.house_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">Street</label>
-                    <input type="text" v-model="permanent_address.street" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">Barangay</label>
-                    <input type="text" v-model="permanent_address.barangay" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">City</label>
-                    <input type="text" v-model="permanent_address.city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">Province</label>
-                    <input type="text" v-model="permanent_address.province" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">Region</label>
-                    <input type="text" v-model="permanent_address.region" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">Country</label>
-                    <input type="text" v-model="permanent_address.country" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-            </div>
-            <div class="mb-4">
-                <p class="block text-gray-700">Current Address</p>
-                <div class="mb-4">
-                    <label class="block text-gray-700">House Number</label>
-                    <input type="text" v-model="current_address.house_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">Street</label>
-                    <input type="text" v-model="current_address.street" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">Barangay</label>
-                    <input type="text" v-model="current_address.barangay" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">City</label>
-                    <input type="text" v-model="current_address.city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">Province</label>
-                    <input type="text" v-model="current_address.province" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">Region</label>
-                    <input type="text" v-model="current_address.region" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700">Country</label>
-                    <input type="text" v-model="current_address.country" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-            </div>
-            <button type="button" @click="handlePrevStep" class="px-4 py-2 font-bold text-white bg-gray-500 rounded hover:bg-gray-700">Previous</button>
-            <button type="submit" class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Next</button>
-        </form>
+    <div class="mt-4">
+      <form @submit.prevent="handleNextStep">
+        
+        <p class="block my-4 text-xl font-bold text-center text-text-400">Permanent Address</p>
+        <div class="grid grid-cols-2 gap-4">
+          <div>
+            <label class="block mb-2 text-sm text-text-default">House Number</label>
+            <input type="text" v-model="permanent_address.house_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block mb-2 text-sm text-text-default">Street</label>
+            <input type="text" v-model="permanent_address.street" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block mb-2 text-sm text-text-default">Barangay</label>
+            <input type="text" v-model="permanent_address.barangay" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block mb-2 text-sm text-text-default">City</label>
+            <input type="text" v-model="permanent_address.city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block mb-2 text-sm text-text-default">Province</label>
+            <input type="text" v-model="permanent_address.province" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block mb-2 text-sm text-text-default">Region</label>
+            <input type="text" v-model="permanent_address.region" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block mb-2 text-sm text-text-default">Country</label>
+            <input type="text" v-model="permanent_address.country" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+        </div>
+        <label class="flex items-center gap-2 my-4 text-text-default">
+          <input type="checkbox" class="size-5" v-model="sameAddress" /> 
+          <span>My current address is the same as my permanent address</span>
+        </label>
+  
+  
+        <p v-if="!sameAddress" class="block my-4 text-xl font-bold text-center text-secondary-default">Current Address</p>
+        <div v-if="!sameAddress" class="grid grid-cols-2 gap-4">
+          <div>
+            <label class="block text-text-default">House Number</label>
+            <input type="text" v-model="current_address.house_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block text-text-default">Street</label>
+            <input type="text" v-model="current_address.street" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block text-text-default">Barangay</label>
+            <input type="text" v-model="current_address.barangay" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block text-text-default">City</label>
+            <input type="text" v-model="current_address.city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block text-text-default">Province</label>
+            <input type="text" v-model="current_address.province" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block text-text-default">Region</label>
+            <input type="text" v-model="current_address.region" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+          <div>
+            <label class="block text-text-default">Country</label>
+            <input type="text" v-model="current_address.country" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          </div>
+        </div>
+        
+        <div class="flex justify-end gap-2 mt-8">
+          <button type="button" @click="handlePrevStep" class="text-sm font-medium bg-secondary-300 text-text-default disabled:bg-primary-200 py-2.5 px-5 rounded-lg text-center">Previous</button>
+          <button type="submit" class="text-sm bg-primary-default font-medium text-text-default disabled:bg-primary-200 py-2.5 px-5 rounded-lg text-center">Next</button>
+        </div>
+      </form>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  import { ref, watch } from 'vue';
+  
+  export default {
     name: 'Step2',
     props: {
-        permanent_address: Object,
-        current_address: Object,
+      permanent_address: Object,
+      current_address: Object,
     },
     setup(props, { emit }) {
-        const handlePrevStep = () => {
-            emit('prevStep')
+      const sameAddress = ref(false);
+  
+      watch(
+        () => sameAddress.value,
+        (newVal) => {
+          if (newVal) {
+            props.current_address.house_number = props.permanent_address.house_number;
+            props.current_address.street = props.permanent_address.street;
+            props.current_address.barangay = props.permanent_address.barangay;
+            props.current_address.city = props.permanent_address.city;
+            props.current_address.province = props.permanent_address.province;
+            props.current_address.region = props.permanent_address.region;
+            props.current_address.country = props.permanent_address.country;
+          }
         }
-
-        const handleNextStep = () => {
-            emit('submit-step2')
-        }
-
-        return {
-            handleNextStep,
-            handlePrevStep,
-        }
-    }   
-}
-</script>
+      );
+  
+      const handlePrevStep = () => {
+        emit('prev-step');
+      };
+  
+      const handleNextStep = () => {
+        console.log(props.permanent_address)
+        console.log(props.current_address)
+        emit('next-step');
+      };
+  
+      return {
+        sameAddress,
+        handleNextStep,
+        handlePrevStep,
+      };
+    },
+  };
+  </script>
+  
